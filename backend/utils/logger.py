2b7c9856c190edf3c1906dd66ef58ae8d0f488af
sharath <29162020+tnfssc@users.noreply.github.com>
2025-07-07 19:42:26 +0000

    chore(logger): add dynamic logging level configuration based on environment variable

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -1,6 +1,9 @@
 import structlog, logging, os
 
 ENV_MODE = os.getenv("ENV_MODE", "LOCAL")
+LOGGING_LEVEL = logging.getLevelNamesMapping().get(
+    os.getenv("LOGGING_LEVEL", "DEBUG").upper(), logging.DEBUG
+)
 
 renderer = [structlog.processors.JSONRenderer()]
 if ENV_MODE.lower() == "local".lower() or ENV_MODE.lower() == "staging".lower():
@@ -23,6 +26,7 @@ structlog.configure(
         *renderer,
     ],
     cache_logger_on_first_use=True,
+    wrapper_class=structlog.make_filtering_bound_logger(LOGGING_LEVEL),
 )
 
-logger: structlog.stdlib.BoundLogger = structlog.get_logger(level=logging.DEBUG)
+logger: structlog.stdlib.BoundLogger = structlog.get_logger()
