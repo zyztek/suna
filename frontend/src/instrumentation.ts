78ebe1e21fa056cccb7bef9bd68f3af19389c5b8
sharath <29162020+tnfssc@users.noreply.github.com>
2025-07-08 17:05:32 +0000

    chore(frontend): remove Sentry integration and related configurations

--- frontend/src/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-import { SentryConfig } from './sentry.config';
-
-export async function register() {
-  Sentry.init(SentryConfig);
-}
-
-export const onRequestError = Sentry.captureRequestError;
