c62b540ce6cdb8cb1ed451e3a0754cf7fe63c8ac
Krishav Raj Singh <krishavrajsingh@gmail.com>
2025-07-18 03:27:23 +0530

    fix: horizontal scroll

--- frontend/src/components/ui/scroll-area.tsx
+++ frontend/src/components/ui/scroll-area.tsx
@@ -23,6 +23,7 @@ function ScrollArea({
         {children}
       </ScrollAreaPrimitive.Viewport>
       <ScrollBar />
+      <ScrollBar orientation="horizontal" />
       <ScrollAreaPrimitive.Corner />
     </ScrollAreaPrimitive.Root>
   );
